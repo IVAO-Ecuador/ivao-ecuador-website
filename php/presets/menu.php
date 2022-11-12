e4b4f5006d74926ab3c0f999c932f37942c17506
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2022-11-12 16:12:25 -0500

    [Correción] Se renombra un enlace correctamente

--- php/presets/menu.php
+++ php/presets/menu.php
@@ -76,7 +76,7 @@
             </p>
             <ul class="children">
               <li><a href="https://wiki.ivao.aero/en/home/training/main/pilot_documentation" target="_blank">Training</a></li>
-              <li><a href="https://ec.ivao.aero/atc/documents/GUÍA-DE-USO-UNICOM-POR-VOZ.pdf" target="_blank">Guidelines for ATC Exams</a></li>
+              <li><a href="https://ec.ivao.aero/atc/documents/GUÍA-DE-USO-UNICOM-POR-VOZ.pdf" target="_blank">Manual de Voice UNICOM</a></li>
             </ul>
           </li>
 
